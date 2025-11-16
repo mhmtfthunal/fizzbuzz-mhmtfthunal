8eb997348ccf50c947f35ca0e9c95350fb5d20f2
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-11-16 15:03:52 +0300

    FizzBuzz fonksiyonlarÄ± implement edildi - if ve switch versiyonlarÄ± tamamlandÄ±

--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,14 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+    switch (true) {
+        case (num % 3 === 0 && num % 5 === 0):
+            return 'FizzBuzz';
+        case (num % 3 === 0):
+            return 'Fizz';
+        case (num % 5 === 0):
+            return 'Buzz';
+        default:
+            return num;
+    }
 }
 
 module.exports = fizzbuzzSwitch;
